956c93f6dde23cf27178a551151b8f5c24a07014
MoRti <themorti@yahoo.com>
2020-05-26 16:20:02 +0430

    fix style of inputs

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 		</head>
 		<body style="background-image: src('Images/loginBg.png');">
 			<h1 id="topText">Append everything Together !</h1>
-			<div id="loginBox" style="height: 11003px;padding: 12px;">
+			<div id="loginBox" style="height: 650px;padding: 12px;">
 				<p id="boxContent" style="margin-top: 25px;">Insert 3 Parameters</p>
 				<form action="result.php" method="post">
-					<textarea type="text" name="one" class="marg" id="emailF" placeholder="1" style="height: 10cm;font-size: 15px;"/></textarea>
+					<textarea type="text" name="one" class="marg" id="emailF" placeholder="1" style="width:30%;height: 10cm;font-size: 15px;"/></textarea>
 					<textarea type="text" name="two" class="marg" id="other" placeholder="2" style="border-bottom: 2pt solid red;height: 10cm;font-size: 15px;"/></textarea>
 					<textarea type="text" name="three" class="marg" id="other" placeholder="3" style="border-bottom: 2pt solid green;height: 10cm;font-size: 15px;"/></textarea>					<br>
 					<br>
