5aee8f9dc13726fe11df9c4413095153ac20dfe5
MoRti <themorti@yahoo.com>
2020-05-27 16:08:12 +0430

    addig if statmenets for each checkbox select

--- result.php
+++ result.php
@@ -8,6 +8,125 @@ $onearray = preg_split('/\r\n|\r|\n/', $one);
 $twoarray = preg_split('/\r\n|\r|\n/', $two);
 $threearray = preg_split('/\r\n|\r|\n/', $three);
 
+if (@$_POST['a1']) {
+	$rep1x = array_map(fn($el) => str_replace('a', '@', $el), $onearray);
+	$rep2x = array_map(fn($el) => str_replace('a', '@', $el), $twoarray);
+	$rep3x = array_map(fn($el) => str_replace('a', '@', $el), $threearray);
+
+	$rep1 = array_map(fn($el) => str_replace('A', '@', $el), $rep1x);
+	$rep2 = array_map(fn($el) => str_replace('A', '@', $el), $rep2x);
+	$rep3 = array_map(fn($el) => str_replace('A', '@', $el), $rep3x);
+
+} elseif (@$_POST['a2']) {
+
+	$rep1x = array_map(fn($el) => str_replace('a', '4', $el), $onearray);
+	$rep2x = array_map(fn($el) => str_replace('a', '4', $el), $twoarray);
+	$rep3x = array_map(fn($el) => str_replace('a', '4', $el), $threearray);
+
+	$rep1 = array_map(fn($el) => str_replace('A', '4', $el), $rep1x);
+	$rep2 = array_map(fn($el) => str_replace('A', '4', $el), $rep2x);
+	$rep3 = array_map(fn($el) => str_replace('A', '4', $el), $rep3x);
+
+}  elseif (@$_POST['e1']) {
+
+	$rep1x = array_map(fn($el) => str_replace('e', '3', $el), $onearray);
+	$rep2x = array_map(fn($el) => str_replace('e', '3', $el), $twoarray);
+	$rep3x = array_map(fn($el) => str_replace('e', '3', $el), $threearray);
+
+	$rep1 = array_map(fn($el) => str_replace('E', '3', $el), $rep1x);
+	$rep2 = array_map(fn($el) => str_replace('E', '3', $el), $rep2x);
+	$rep3 = array_map(fn($el) => str_replace('E', '3', $el), $rep3x);
+	
+}  elseif (@$_POST['s1']) {
+
+	$rep1x = array_map(fn($el) => str_replace('s', '$', $el), $onearray);
+	$rep2x = array_map(fn($el) => str_replace('s', '$', $el), $twoarray);
+	$rep3x = array_map(fn($el) => str_replace('s', '$', $el), $threearray);
+
+	$rep1 = array_map(fn($el) => str_replace('S', '$', $el), $rep1x);
+	$rep2 = array_map(fn($el) => str_replace('S', '$', $el), $rep2x);
+	$rep3 = array_map(fn($el) => str_replace('S', '$', $el), $rep3x);
+	
+}  elseif (@$_POST['s2']) {
+
+	$rep1x = array_map(fn($el) => str_replace('s', '5', $el), $onearray);
+	$rep2x = array_map(fn($el) => str_replace('s', '5', $el), $twoarray);
+	$rep3x = array_map(fn($el) => str_replace('s', '5', $el), $threearray);
+
+	$rep1 = array_map(fn($el) => str_replace('S', '5', $el), $rep1x);
+	$rep2 = array_map(fn($el) => str_replace('S', '5', $el), $rep2x);
+	$rep3 = array_map(fn($el) => str_replace('S', '5', $el), $rep3x);
+	
+}  elseif (@$_POST['o1']) {
+
+	$rep1x = array_map(fn($el) => str_replace('o', '0', $el), $onearray);
+	$rep2x = array_map(fn($el) => str_replace('o', '0', $el), $twoarray);
+	$rep3x = array_map(fn($el) => str_replace('o', '0', $el), $threearray);
+
+	$rep1 = array_map(fn($el) => str_replace('O', '0', $el), $rep1x);
+	$rep2 = array_map(fn($el) => str_replace('O', '0', $el), $rep2x);
+	$rep3 = array_map(fn($el) => str_replace('O', '0', $el), $rep3x);
+	
+}  elseif (@$_POST['t1']) {
+
+	$rep1x = array_map(fn($el) => str_replace('t', '7', $el), $onearray);
+	$rep2x = array_map(fn($el) => str_replace('t', '7', $el), $twoarray);
+	$rep3x = array_map(fn($el) => str_replace('t', '7', $el), $threearray);
+
+	$rep1 = array_map(fn($el) => str_replace('T', '7', $el), $rep1x);
+	$rep2 = array_map(fn($el) => str_replace('T', '7', $el), $rep2x);
+	$rep3 = array_map(fn($el) => str_replace('T', '7', $el), $rep3x);
+	
+}  elseif (@$_POST['i1']) {
+
+	$rep1x = array_map(fn($el) => str_replace('i', '!', $el), $onearray);
+	$rep2x = array_map(fn($el) => str_replace('i', '!', $el), $twoarray);
+	$rep3x = array_map(fn($el) => str_replace('i', '!', $el), $threearray);
+
+	$rep1 = array_map(fn($el) => str_replace('I', '!', $el), $rep1x);
+	$rep2 = array_map(fn($el) => str_replace('I', '!', $el), $rep2x);
+	$rep3 = array_map(fn($el) => str_replace('I', '!', $el), $rep3x);	
+
+}  elseif (@$_POST['i2']) {
+
+	$rep1x = array_map(fn($el) => str_replace('i', '1', $el), $onearray);
+	$rep2x = array_map(fn($el) => str_replace('i', '1', $el), $twoarray);
+	$rep3x = array_map(fn($el) => str_replace('i', '1', $el), $threearray);
+
+	$rep1 = array_map(fn($el) => str_replace('I', '1', $el), $rep1x);
+	$rep2 = array_map(fn($el) => str_replace('I', '1', $el), $rep2x);
+	$rep3 = array_map(fn($el) => str_replace('I', '1', $el), $rep3x);
+	
+}  elseif (@$_POST['l1']) {
+
+	$rep1x = array_map(fn($el) => str_replace('l', '1', $el), $onearray);
+	$rep2x = array_map(fn($el) => str_replace('l', '1', $el), $twoarray);
+	$rep3x = array_map(fn($el) => str_replace('l', '1', $el), $threearray);
+
+	$rep1 = array_map(fn($el) => str_replace('L', '1', $el), $rep1x);
+	$rep2 = array_map(fn($el) => str_replace('L', '1', $el), $rep2x);
+	$rep3 = array_map(fn($el) => str_replace('L', '1', $el), $rep3x);
+	
+}  elseif (@$_POST['w1']) {
+
+	$rep1x = array_map(fn($el) => str_replace('w', 'vv', $el), $onearray);
+	$rep2x = array_map(fn($el) => str_replace('w', 'vv', $el), $twoarray);
+	$rep3x = array_map(fn($el) => str_replace('w', 'vv', $el), $threearray);
+
+	$rep1 = array_map(fn($el) => str_replace('W', 'vv', $el), $rep1x);
+	$rep2 = array_map(fn($el) => str_replace('W', 'vv', $el), $rep2x);
+	$rep3 = array_map(fn($el) => str_replace('W', 'vv', $el), $rep3x);
+	
+}  elseif (@$_POST['g1']) {
+
+	$rep1x = array_map(fn($el) => str_replace('g', '6', $el), $onearray);
+	$rep2x = array_map(fn($el) => str_replace('g', '6', $el), $twoarray);
+	$rep3x = array_map(fn($el) => str_replace('g', '6', $el), $threearray);
+
+	$rep1 = array_map(fn($el) => str_replace('G', '6', $el), $rep1x);
+	$rep2 = array_map(fn($el) => str_replace('G', '6', $el), $rep2x);
+	$rep3 = array_map(fn($el) => str_replace('G', '6', $el), $rep3x);	
+}		
 ?>
 <!DOCTYPE html>
 	<html>
@@ -24,16 +143,16 @@ $threearray = preg_split('/\r\n|\r|\n/', $three);
 					<textarea type="text" rows="4" cols="50" class="marg" id="emailF" style="border: 1.7pt solid #2196F3;font-size: 18px; width: 60%; height: 10cm;"/><?php
 						$i = 0;
 						// get indexes numbers of first input
-						$countarr1 = count($onearray);
+						$countarr1 = count($rep1);
 						//$c = array_combine($twoarray, $threearray);
 						
 						for($i = 0; $i < $countarr1; $i++){
 							// showing all values of second input values
-							foreach ($twoarray as $val2) {
+							foreach ($rep2 as $val2) {
 								// showing all values of third input values
-								foreach ($threearray as $val3) {
+								foreach ($rep3 as $val3) {
 								// append proccess 
-								@$resultarr[$i] = @$onearray[$i].@$val2.@$val3;
+								@$resultarr[$i] = @$rep1[$i].@$val2.@$val3;
 								echo @$resultarr[$i]."\r\n";
 								}
 							}	
